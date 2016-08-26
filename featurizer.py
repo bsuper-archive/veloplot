a75da1d4466a6e420d08f54daf9e7a086cf87bf9
Brian Su <briansuaccs@gmail.com>
2016-08-26 12:10:42 -0700

    Formatted all Python files using YAPF.

--- featurizer.py
+++ featurizer.py
@@ -5,6 +5,7 @@ import itertools
 import glob
 import pandas as pd
 
+
 def segment(data, window_size=512, overlap=.5, padding=None):
     """
     Segment data by WINDOW SIZE with OVERLAP
@@ -18,8 +19,10 @@ def segment(data, window_size=512, overlap=.5, padding=None):
         windows.append(data[i:i + window_size])
     return windows
 
+
 SAMPLING_FREQUENCY = 1000.
 SAMPLING_INTERVAL = 1. / SAMPLING_FREQUENCY
+
 
 def fft_freq(data, window_func=signal.hanning):
     w = window_func(data.size)
@@ -29,6 +32,7 @@ def fft_freq(data, window_func=signal.hanning):
     sig_fft = sig_fft[range(data.size / 2)]
     return sig_fft, freq
 
+
 def rfft_freq(data, window_func=signal.hanning):
     w = window_func(data.size)
     sig_fft = fftpack.rfft(data * w)
@@ -37,23 +41,28 @@ def rfft_freq(data, window_func=signal.hanning):
     sig_fft = sig_fft[range(data.size / 2)]
     return sig_fft, freq
 
+
 def freq_domain_entropy(sig_fft):
-    psd = (sig_fft ** 2) / sig_fft.shape[0]
+    psd = (sig_fft**2) / sig_fft.shape[0]
     psd = psd / np.sum(psd)
     psd = map(lambda p: -p * np.log(p), psd)
     return np.sum(psd)
 
+
 def energy(sig_fft):
-    return np.sum(np.abs(sig_fft) ** 2)
+    return np.sum(np.abs(sig_fft)**2)
+
 
 def pairwise_correlation(a, b):
     return np.correlate(a, b)
+
 
 def featurize(df_seg):
     features = []
 
     cols = ["Fx", "Fy", "Fz", "F_mag", "Mx", "My", "Mz", "M_mag"]
-    cols = ["Fx", "Fy", "Fz", "F_mag", "Mx", "My", "Mz", "M_mag", "AX", "AY", "AZ", "A_mag"]
+    cols = ["Fx", "Fy", "Fz", "F_mag", "Mx", "My", "Mz", "M_mag", "AX", "AY",
+            "AZ", "A_mag"]
     for col in cols:
         # statistical metrics
         features.append(np.max(df_seg[col]))
@@ -75,15 +84,18 @@ def featurize(df_seg):
 
     for col_set in [f_cols, m_cols, a_cols]:
         for pair in itertools.combinations(col_set, 2):
-            features.append(pairwise_correlation(df_seg[pair[0]], df_seg[pair[1]])[0])
+            features.append(
+                pairwise_correlation(df_seg[pair[0]], df_seg[pair[1]])[0])
 
     return np.array(features)
+
 
 def get_feature_names():
     features = []
 
     cols = ["Fx", "Fy", "Fz", "F_mag", "Mx", "My", "Mz", "M_mag"]
-    cols = ["Fx", "Fy", "Fz", "F_mag", "Mx", "My", "Mz", "M_mag", "AX", "AY", "AZ", "A_mag"]
+    cols = ["Fx", "Fy", "Fz", "F_mag", "Mx", "My", "Mz", "M_mag", "AX", "AY",
+            "AZ", "A_mag"]
     for col in cols:
         # statistical metrics
         features.append("{0}_max".format(col))
@@ -112,6 +124,7 @@ def get_feature_names():
 # PERFORM FEATURIZATION
 #########################################
 
+
 def create_feature_vector(segmented_data_files, label=0):
     lst = []
     labels = []
@@ -123,8 +136,10 @@ def create_feature_vector(segmented_data_files, label=0):
             labels.append(label)
     return np.array(lst), np.array(labels)
 
+
 def get_feature_vector(ctl_files, act_files):
     ctl_features, ctl_labels = create_feature_vector(ctl_files, label=0)
     act_features, act_labels = create_feature_vector(act_files, label=1)
     print "CTL size:", ctl_labels.shape[0], "ACT size:", act_labels.shape[0]
-    return np.vstack((ctl_features, act_features)), np.hstack((ctl_labels, act_labels))
+    return np.vstack((ctl_features, act_features)), np.hstack(
+        (ctl_labels, act_labels))
